511fbc113ea0d2d6db97d3196e43b348e78f2c2e
subha2087 <60758516+subha2087@users.noreply.github.com>
2020-12-13 18:37:06 +0000

    Unit Test Project added

---
+++ QuickFixDental/UnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestProject")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f1ec6c6-1ac6-4719-81af-a670b9c85da4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ QuickFixDental/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickFixDental.BusinessLogic;
+using QuickFixDental.Model;
+using QuickFixDental;
+using System;
+using Moq;
+using System.Data.Entity;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestProject
+{
+    /// <summary>
+    ///Reference: https://docs.microsoft.com/en-us/ef/ef6/fundamentals/testing/mocking
+    /// </summary>
+
+    [TestClass]
+    public class UnitTest1
+    {
+        PatientBL patientBL;
+
+        [TestMethod]
+        public void AddPatientTest()
+        {
+            var mockSet = new Mock<DbSet<Patient>>();
+            var mockContext = new Mock<MyDBEntities>();
+            mockContext.Setup(m => m.Patients).Returns(mockSet.Object);
+            patientBL = new PatientBL(mockContext.Object);
+            var Patient = new Patient();
+            Patient.Name = "Test Patient";
+            Patient.Address = "Test Address";
+            Patient.DOB = DateTime.Now;
+            Patient.Email = "asaasf.sds.com";
+            Patient.GPName = "xyx";
+            Patient.GPAddress = "dsfdsf";
+            var ret=patientBL.AddPatient(Patient);
+            mockSet.Verify(m => m.Add(It.IsAny<Patient>()), Times.Once());
+            mockContext.Verify(m => m.SaveChanges(), Times.Once());
+        }
+
+        [TestMethod]
+        public void GetPatientsTest()
+        {
+            var data = new List<Patient>
+            {
+                new Patient { Name = "AAA" },
+                new Patient { Name = "BBB" },
+                new Patient { Name = "ZZZ" },
+            }.AsQueryable();
+
+            var mockSet = new Mock<DbSet<Patient>>();
+            mockSet.As<IQueryable<Patient>>().Setup(m => m.Provider).Returns(data.Provider);
+            mockSet.As<IQueryable<Patient>>().Setup(m => m.Expression).Returns(data.Expression);
+            mockSet.As<IQueryable<Patient>>().Setup(m => m.ElementType).Returns(data.ElementType);
+            mockSet.As<IQueryable<Patient>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+
+            var mockContext = new Mock<MyDBEntities>();
+            mockContext.Setup(c => c.Patients).Returns(mockSet.Object);
+
+            var service = new PatientBL(mockContext.Object);
+            var patients = service.GetPatients();
+
+            Assert.AreEqual(3, patients.Count);
+            Assert.AreEqual("AAA", patients[0].Name);
+            Assert.AreEqual("BBB", patients[1].Name);
+            Assert.AreEqual("ZZZ", patients[2].Name);
+        }
+    }
+}
